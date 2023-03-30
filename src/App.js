53a026999d42de8ad0af973b97d1243003f6a473
janetuwili <janetuwili@gmail.com>
2023-03-29 23:53:50 -0700

    jeannette first push

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import SignUp from './pages/SignUp';
 import { Routes, Route } from 'react-router-dom';
+import SignIn from './pages/SignIn';
 function App() {
   return (
-    <SignUp/>
+    <>
+    
+    <SignIn/>
+    </>
  
   );
 }
