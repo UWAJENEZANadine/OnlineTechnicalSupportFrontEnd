0d8170253a6777710abe36c68f97fd1d4abe6797
janetuwili <janetuwili@gmail.com>
2023-04-04 00:43:44 -0700

    changes i made

--- src/App.js
+++ src/App.js
@@ -3,16 +3,26 @@ import "./App.css";
 import SignUp from "./pages/SignUp";
 import { Routes, Route } from "react-router-dom";
 import SignIn from "./pages/SignIn";
+import Header from "./Component/Header";
+import HomePage from "./pages/HomePage";
+import Footer from "./Component/Footer";
 function App() {
   return (
+
     <>
-    <Routes>
-      <Route path="" element={<SignUp/>} />
-      <Route path="SignUp" element={<SignUp/>} />
-      <Route path="SignIn" element={<SignIn/>} />
-    </Routes>
-    
-      </>
+      <Header />
+      <Routes>
+        <Route path="" element={<HomePage />} />
+        <Route path="HomePage" element={<HomePage />} />
+        <Route path="SignUp" element={<SignUp />} />
+        <Route path="SignIn" element={<SignIn />} />
+
+
+
+      </Routes>
+      <Footer />
+
+    </>
   );
 }
 
