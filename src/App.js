1ed6e9331ec85e2029e2116b90e9bf1b37805560
UWAJENEZANadine <adelineu640@gmail.com>
2023-04-04 09:51:19 +0200

    dashboard

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import logo from "./logo.svg";
 import "./App.css";
 import SignUp from "./pages/SignUp";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import SignIn from "./pages/SignIn";
+// import Dashboard from "./components/dashboard/pages/Dashboard";
+import SideNav from "./components/dashboard/sideNav";
+
+// const isUserLogedIn = localStorage.getItem("userLogedIn");
+
 function App() {
+  // const currentUrl = useLocation().pathname;
   return (
     <>
-    <Routes>
-      <Route path="" element={<SignUp/>} />
-      <Route path="SignUp" element={<SignUp/>} />
-      <Route path="SignIn" element={<SignIn/>} />
-    </Routes>
-    
-      </>
+      <Routes>
+        <Route path="" element={<SignUp />} />
+        <Route path="SignUp" element={<SignUp />} />
+        <Route path="SignIn" element={<SignIn />} />
+        <Route path="dashboard" element={<SideNav/>} />
+
+      </Routes>
+      {/* {isUserLogedIn && currentUrl.includes("/dash") ? (
+        <SideNav>
+          <Routes>
+            <Route path="/dash/dashboard" element={<Dashboard />} />
+          </Routes>
+        </SideNav>
+      ) : (
+        <></>
+      )} */}
+    </>
   );
 }
 
---
+++ src/components/dashboard/pages/schedulers.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Schedulers= ()=> {
+  return (
+    <div>schedulers</div>
+  )
+}
+
+export default Schedulers
